8a5d161171299882b53b009855227d4f42fe03fe
Ilia Bozhinov <ammen99@gmail.com>
2022-07-31 21:58:24 +0200

    switch test clients to meson build system and auto-build them

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-PYTHONPATH=$PYTHONPATH:$(pwd)/wfpytest/ PATH=$PATH:$(pwd)/clients ./run_tests.py "$@"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Build clients first
+(
+cd $SCRIPT_DIR/clients
+if ! [ -d build/ ]; then
+    meson build
+fi
+ninja -C build
+)
+
+PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/wfpytest/ PATH=$PATH:$SCRIPT_DIR/clients/build $SCRIPT_DIR/run_tests.py "$@"
