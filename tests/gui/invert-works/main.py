290922d2cb7f02f7dc192b479aa745fb11f04448
Ilia Bozhinov <ammen99@gmail.com>
2022-08-18 11:33:15 +0200

    tests: add simple test for inverted colors

---
+++ tests/gui/invert-works/main.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+
+import wftest as wt
+
+def is_gui() -> bool:
+    return True
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['wleird-gamma-blend'])
+
+    def _run(self):
+        self.socket.run('wleird-gamma-blend')
+        self.wait_for_clients()
+        self.socket.press_key('KEY_I')
+        self.wait_for_clients()
+
+        return wt.Status.OK, None
