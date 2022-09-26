6463403d5fd521283b7f9c39749cdfb4845b90bd
Ilia Bozhinov <ammen99@gmail.com>
2022-09-26 21:07:15 +0200

    xwayland: add test for starting gitk with its menus

---
+++ tests/xwayland/gitk-menus/main.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+import wftest as wt
+
+def is_gui() -> bool:
+    return True
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['gitk'])
+
+    def _run(self):
+        self.socket.run('gitk')
+        self.wait_for_clients(2)
+
+        layout = {}
+        layout['Gitk'] = (0, 0, 500, 500)
+        self.socket.layout_views(layout)
+        self.wait_for_clients(2)
+        if not self.socket.get_view_info('Gitk'):
+            return wt.Status.WRONG, 'Gitk not running?'
+
+        # Move around a bit, see that wayfire doesn't crash
+        self.socket.move_cursor(10, 10)
+        self.socket.click_button('BTN_LEFT', 'full')
+        self.wait_for_clients(2)
+        self.socket.move_cursor(100, 10)
+        self.wait_for_clients(2)
+
+        if not self.socket.get_view_info('Gitk'):
+            return wt.Status.WRONG, 'Gitk not running?'
+
+        if err := self.take_screenshot('final'):
+            return wt.Status.CRASHED, err
+
+        return wt.Status.OK, None
