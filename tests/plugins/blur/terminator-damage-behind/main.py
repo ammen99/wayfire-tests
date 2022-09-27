3341251408b9cd0ecc084213c2ffd9a91b2f5d54
Ilia Bozhinov <ammen99@gmail.com>
2022-09-27 23:05:43 +0200

    blur: add terminator-damage-behind test

---
+++ tests/plugins/blur/terminator-damage-behind/main.py
@@ -0,0 +1,42 @@
+#!/bin/env python3
+
+import wftest as wt
+import os
+import signal
+
+def is_gui() -> bool:
+    return True
+
+# This client tiles weston-terminal two times and opens a view on top.
+# Then it proceeds to check that the whole sublayer is moved to the top
+# when clicking on the tiled views.
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['terminator', 'gtk_color_switcher'])
+
+    def _get_views(self):
+        return sorted([v['title'] for v in self.socket.list_views()])
+
+    def _run(self):
+        gcs_pid = self.socket.run('gtk_color_switcher gcs')['pid']
+        self.wait_for_clients(2)
+
+        layout = {}
+        layout['gcs'] = (250, 250, 500, 500)
+        self.socket.layout_views(layout)
+        self.wait_for_clients(2)
+
+        self.socket.run('terminator -m')
+        self.socket.run('terminator -m')
+        self.wait_for_clients(4) # Terminator startup may be slow
+
+        if error := self.take_screenshot('scene-setup'):
+            return wt.Status.CRASHED, error
+
+        # Damage behind terminator and see that blur expands damage
+        os.kill(gcs_pid, signal.SIGUSR1)
+        self.wait_for_clients(2)
+
+        if error := self.take_screenshot('final'):
+            return wt.Status.CRASHED, error
+        return wt.Status.OK, None
