1f0bd0dd6879ca1426d15f3b3bd714a479a44671
Ilia Bozhinov <ammen99@gmail.com>
2022-09-28 10:59:34 +0200

    add blur/dragged-view

---
+++ tests/plugins/blur/dragged-view/main.py
@@ -0,0 +1,48 @@
+#!/bin/env python3
+
+import wftest as wt
+import os
+import signal
+
+# The test requires to compare two blur images.
+# The blur effect is not very stable, so wide, but small
+# changes are expected. Therefore, we need a higher sensitivity
+# for this test.
+def sensitivity():
+    return 0.0
+
+def is_gui() -> bool:
+    return True
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal', 'wf-background'])
+
+    def _get_views(self):
+        return sorted([v['title'] for v in self.socket.list_views()])
+
+    def _run(self):
+        wt_pid = self.socket.run('weston-terminal')["pid"]
+        self.socket.run('wf-background')
+        self.socket.create_wayland_output()
+        self.wait_ms(1000) # Wait for wf-background to start and be initialized
+
+        layout = {}
+        layout['nil'] = (0, 0, 500, 500, 'WL-1')
+        self.socket.layout_views(layout)
+        self.wait_for_clients(2)
+
+        self.socket.move_cursor(5, 5)
+        self.socket.click_button('BTN_LEFT', 'press')
+
+        self.socket.move_cursor(200, 200)
+        self.wait_for_clients()
+        if error := self.take_screenshot('in-flight'):
+            return wt.Status.CRASHED, error
+
+        os.kill(wt_pid, signal.SIGKILL)
+        self.wait_for_clients(2)
+        if error := self.take_screenshot('force-closed'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
