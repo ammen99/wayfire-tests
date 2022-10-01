a6ca8cc3cc91f8181a9ce64221dccb3900116526
Ilia Bozhinov <ammen99@gmail.com>
2022-10-01 21:27:10 +0200

    blur: add load-unload test

---
+++ tests/plugins/blur/load-unload/main.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import wftest as wt
+import shutil
+
+def is_gui() -> bool:
+    return False
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['terminator'])
+
+    def _get_views(self):
+        return sorted([v['title'] for v in self.socket.list_views()])
+
+    def _run(self):
+        self.socket.run('terminator')
+        self.wait_for_clients(4) # Terminator has a slow startup
+
+        shutil.copyfile('wayfire-no-blur.ini', 'wayfire.ini')
+        self.wait_for_clients()
+        shutil.copyfile('wayfire-blur.ini', 'wayfire.ini')
+        self.wait_for_clients()
+        shutil.copyfile('wayfire-no-blur.ini', 'wayfire.ini')
+        self.wait_for_clients()
+        shutil.copyfile('wayfire-blur.ini', 'wayfire.ini')
+        self.wait_for_clients()
+
+        return wt.Status.OK, None
