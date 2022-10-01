c649e05d4c0e4473f0615973a90860aa74577f62
Ilia Bozhinov <ammen99@gmail.com>
2022-10-01 21:46:47 +0200

    tests: add minimized-is-hidden

---
+++ tests/gui/minimized-is-hidden/main.py
@@ -0,0 +1,21 @@
+#!/bin/env python3
+
+import wftest as wt
+def is_gui() -> bool:
+    return True
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal'])
+
+    def _run(self):
+        self.socket.run('weston-terminal')
+        self.wait_for_clients(2)
+
+        self.socket.press_key('KEY_M')
+        self.wait_for_clients()
+
+        if error := self.take_screenshot('minimized-hidden'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
