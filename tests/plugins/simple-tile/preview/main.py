457f18cb072b90de03fd1b8cb3bfd2f89c52b661
Ilia Bozhinov <ammen99@gmail.com>
2022-10-19 19:15:15 +0200

    add simple-tile/preview test

---
+++ tests/plugins/simple-tile/preview/main.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+import wftest as wt
+
+def is_gui() -> bool:
+    return True
+
+# Simple test which starts weston-terminal maximized, then closes it by clicking on the close button
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal'])
+
+    def _run(self):
+        self.socket.run('weston-terminal')
+        self.socket.run('weston-terminal')
+        self.wait_for_clients(4)
+
+        self.socket.move_cursor(150, 150)
+        self.socket.click_button('BTN_LEFT', 'press')
+        self.socket.move_cursor(255, 255)
+        self.wait_ms(300) # Preview is hardcoded for 200ms, wait a bit more
+
+        if error := self.take_screenshot('preview-shown'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
