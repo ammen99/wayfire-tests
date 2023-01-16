71e04a0f9432e6801fa05f5251f97bd9d5293237
Ilia Bozhinov <ammen99@gmail.com>
2023-01-16 21:57:27 +0100

    scale: check that dragging works

---
+++ tests/plugins/scale/dragging-works/main.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+import wftest as wt
+
+def is_gui() -> bool:
+    return True
+
+# This test opens weston-terminal, starts scale and checks that we can drag the view
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal'])
+
+    def _run(self):
+        self.socket.run('weston-terminal')
+        self.wait_for_clients(2)
+
+        self.socket.press_key('KEY_T')
+        self.wait_for_clients()
+
+        # weston-terminal should be at the center
+        self.socket.move_cursor(250, 250)
+        self.socket.click_button('BTN_LEFT', 'press')
+        self.socket.move_cursor(251, 250) # Trigger movement
+        self.socket.move_cursor(450, 450)
+        self.wait_for_clients()
+
+        if error := self.take_screenshot('1-scale-in-drag'):
+            return wt.Status.CRASHED, error
+
+        self.socket.click_button('BTN_LEFT', 'release')
+        self.wait_for_clients()
+
+        if error := self.take_screenshot('2-scale-drag-released'):
+            return wt.Status.CRASHED, error
+
+        # Close scale
+        self.socket.move_cursor(250, 250)
+        self.socket.click_button('BTN_LEFT', 'full')
+        self.wait_for_clients()
+
+        if error := self.take_screenshot('3-scale-ended'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
