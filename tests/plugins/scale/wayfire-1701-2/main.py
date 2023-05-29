41d29dd7493941eee604656d9631c74adc3fe894
Ilia Bozhinov <ammen99@gmail.com>
2023-05-29 22:47:32 +0200

    add scale/wayfire-1701-2

---
+++ tests/plugins/scale/wayfire-1701-2/main.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+
+import wftest as wt
+import os
+import signal
+
+def is_gui() -> bool:
+    return True
+
+# Wayfire issue #1701
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal'])
+
+    def _run(self):
+        pid = self.socket.run('weston-terminal')['pid']
+        self.wait_for_clients(2)
+
+        # Toggle scale once
+        self.socket.press_key('KEY_S')
+        self.wait_for_clients()
+
+        os.kill(pid, signal.SIGKILL)
+        self.wait_for_clients(10)
+
+        # Open a new window
+        self.socket.run('weston-terminal')
+        self.wait_for_clients(10)
+
+        if error := self.take_screenshot('scale-does-not-show-anything'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
