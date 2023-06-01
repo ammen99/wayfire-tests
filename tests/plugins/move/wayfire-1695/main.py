efbecd0cabb4a49cb0decdc78833008b6c6cb183
Ilia Bozhinov <ammen99@gmail.com>
2023-06-01 10:19:28 +0200

    move: update wayfire-1695 test
    It now checks more cases and uses continuous movement

--- tests/plugins/move/wayfire-1695/main.py
+++ tests/plugins/move/wayfire-1695/main.py
@@ -10,6 +10,16 @@ class WTest(wt.WayfireTest):
     def prepare(self):
         return self.require_test_clients(['weston-terminal'])
 
+    def click_and_drag(self, button, start_x, start_y, end_x, end_y):
+        dx = end_x - start_x
+        dy = end_y - start_y
+
+        self.socket.move_cursor(start_x, start_y)
+        self.socket.click_button(button, 'press')
+        for i in range(11):
+            self.socket.move_cursor(start_x + dx * i // 10, start_y + dy * i // 10)
+        self.socket.click_button(button, 'release')
+
     def _run(self):
         self.socket.create_wayland_output()
         self.socket.run('weston-terminal')
@@ -20,14 +30,18 @@ class WTest(wt.WayfireTest):
         self.socket.layout_views(layout)
         self.wait_for_clients(2)
 
-        self.socket.move_cursor(50, 50)
-        self.socket.click_button('BTN_LEFT', 'press')
-        self.socket.move_cursor(750, 50)
-        self.socket.click_button('BTN_LEFT', 'release')
-        self.wait_for_clients(2)
+        for i in range(2):
+            self.click_and_drag('BTN_RIGHT', 50, 50, 750, 60) # Add 200, 10 to the view geometry
+            g = self.socket.get_view_info('nil')['geometry']
+            if g['x'] != 200 or g['y'] != 10:
+                return wt.Status.WRONG, f'Try #{i}: weston-terminal has invalid geometry after dragging with binding: {str(g)}'
 
-        g = self.socket.get_view_info('nil')['geometry']
-        if g['x'] != 200 or g['y'] != 0:
-            return wt.Status.WRONG, 'weston-terminal has invalid geometry: ' + str(g)
+            # Drag via titlebar this time, back to the first output
+            sx = 500 + g['x'] + 10
+            sy = g['y'] + 10
+            self.click_and_drag('BTN_LEFT', sx, sy, 10, 10)
+            g = self.socket.get_view_info('nil')['geometry']
+            if g['x'] != 0 or g['y'] != 0:
+                return wt.Status.WRONG, f'Try #{i}: weston-terminal has invalid geometry after dragging by titlebar: {str(g)}'
 
         return wt.Status.OK, None
