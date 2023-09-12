605ba2146ac8c0670424f89e0f20c36fb08e1e37
Ilia Bozhinov <ammen99@gmail.com>
2023-09-12 17:33:22 +0200

    add wayfire-1895

---
+++ tests/multi-output/wayfire-1895/main.py
@@ -0,0 +1,32 @@
+#!/bin/env python3
+
+import wftest as wt
+import shutil
+
+def is_gui() -> bool:
+    return False
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['terminator'])
+
+    def _get_views(self):
+        return sorted([v['title'] for v in self.socket.list_views()])
+
+    def run(self, *args, **kwargs):
+        # Make sure WL-1 is enabled when starting wayfire
+        shutil.copyfile('wl1-enabled.ini', 'wayfire.ini')
+        return super().run(*args, **kwargs)
+
+    def _run(self):
+        shutil.copyfile('wl1-disabled.ini', 'wayfire.ini')
+        self.wait_for_clients(2)
+        if not self.socket.ping():
+            return wt.Status.CRASHED, 'Wayfire crashed after disabling?'
+
+        shutil.copyfile('wl1-enabled.ini', 'wayfire.ini')
+        self.wait_for_clients(2)
+        if not self.socket.ping():
+            return wt.Status.CRASHED, 'Wayfire crashed after enabling?'
+
+        return wt.Status.OK, None
