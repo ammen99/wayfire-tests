07793e2e05f18863459f7bc28117c6b572527fbf
Ilia Bozhinov <ammen99@gmail.com>
2023-10-02 08:56:10 +0200

    add switcher/close-view-while-active

---
+++ tests/plugins/switcher/close-view-while-active/main.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import wftest as wt
+import os
+import signal
+
+def is_gui() -> bool:
+    return True
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['gtk_color_switcher', 'weston-terminal'])
+
+    def _run(self):
+        wt_pid = self.socket.run('weston-terminal')['pid']
+        self.socket.run('gtk_color_switcher a')
+        self.wait_for_clients_to_open(nr_clients=2)
+
+        self.socket.press_key('KEY_N')
+        if error := self.take_screenshot('switcher-active'):
+            return wt.Status.CRASHED, error
+
+        os.kill(wt_pid, signal.SIGKILL)
+        self.wait_for_clients()
+
+        if error := self.take_screenshot('switcher-active-one-fewer'):
+            return wt.Status.CRASHED, error
+
+        return wt.Status.OK, None
