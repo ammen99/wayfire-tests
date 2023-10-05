60ca1cfca9a6c970f60a8198f7979b42a3fbcebc
Ilia Bozhinov <ammen99@gmail.com>
2023-10-05 22:06:15 +0200

    run_nested.sh: use cut instead of choose
    This is more portable

--- run_nested.sh
+++ run_nested.sh
@@ -8,7 +8,7 @@ unset WAYLAND_DISPLAY
 
 WLR_RENDER_DRM_DEVICE=/dev/dri/renderD128 WLR_BACKENDS=headless wayfire -c miniconfig.ini &> /tmp/outerlog &
 sleep 1
-display=$(cat /tmp/outerlog | grep "Using socket name" | choose -1 | sed 's/.$//')
+display=$(cat /tmp/outerlog | grep "Using socket name" | cut -d ' ' -f 9)
 
 rm -rf $1/**/*.png
 rm -rf $1/**/*.log
