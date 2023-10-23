08311365c3eb6f92ccf88d50600d6bb2e437a365
Ilia Bozhinov <ammen99@gmail.com>
2023-10-23 17:11:58 +0200

    gtk_special: fixes

--- clients/gtk_special.cpp
+++ clients/gtk_special.cpp
@@ -5,10 +5,11 @@
 // In addition, keyboard interaction is supported too:
 // q -> close window and subwindows
 
-// ./gtk_click_to_close a -> Just main window with title a
-// ./gtk_click_to_close a b -> Main window with title a with dialog b
-// ./gtk_click_to_close a b c -> Main window with title a with dialogs b, c
-// ./gtk_click_to_close a b c d -> Main window with title a with dialogs b, c, c has nested dialog d
+// ./gtk_special a -> Just main window with title a
+// ./gtk_special a b -> Main window with title a with dialog b
+// ./gtk_special a b c -> Main window with title a with dialogs b, c
+// ./gtk_special a b c d -> Main window with title a with dialogs b, c, c has nested dialog d
+#include "glibmm/main.h"
 #include <gtkmm.h>
 
 int cnt_created = 0;
@@ -53,6 +54,8 @@ static void setup_window(Gtk::Window *win)
             ;
         }
     });
+
+    win->show_all();
 }
 
 int main(int argc, char **argv)
@@ -63,35 +66,34 @@ int main(int argc, char **argv)
 
     a = new Gtk::Window;
     a->set_default_size(200, 200);
-    setup_window(a);
     a->set_title(argv[1]);
-    a->show_all();
+    setup_window(a);
 
-    if (argc >= 3) {
-        b = new Gtk::MessageDialog(*a, "Test");
-        b->set_title(argv[2]);
-        setup_window(b);
-    }
-
-    if (argc >= 4) {
-        c = new Gtk::MessageDialog(*a, "Test");
-        c->set_title(argv[3]);
-        setup_window(c);
-    }
-
-    if (argc >= 5) {
-        d = new Gtk::MessageDialog(*c, "Test");
-        d->set_title(argv[4]);
-        setup_window(d);
-    }
-
-    for (auto x : {a, b, c, d})
+    // Create dialogs after a timeout.
+    // Without a timeout, GTK3 has a bug: it sets the parent too early, so that it is unmapped, which according
+    // to the xdg spec is a no-op
+    Glib::signal_timeout().connect_once([&] ()
     {
-        if (x)
-        {
-            x->show_all();
+        if (argc >= 3) {
+            b = new Gtk::MessageDialog(*a, "Test");
+            b->set_title(argv[2]);
+            setup_window(b);
         }
-    }
+
+        if (argc >= 4) {
+            c = new Gtk::MessageDialog(*a, "Test");
+            c->set_title(argv[3]);
+            setup_window(c);
+        }
+
+        Glib::signal_timeout().connect_once([&] () {
+            if (argc >= 5) {
+                d = new Gtk::MessageDialog(*c, "Test");
+                d->set_title(argv[4]);
+                setup_window(d);
+            }
+        }, 50);
+    }, 50);
 
     app->run(*a);
     return 0;
