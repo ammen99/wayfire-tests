85a8e207ad5b0f5036e77f0c3a9469a4624e244c
Ilia Bozhinov <ammen99@gmail.com>
2023-10-29 21:48:37 +0100

    add wayfire-1983

---
+++ tests/plugins/simple-tile/wayfire-1983/main.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+import wftest as wt
+import wfipclib as wi
+
+def is_gui() -> bool:
+    return False
+
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal', 'wf-panel'])
+
+    def _run(self):
+        self.socket.run('weston-terminal')
+        self.wait_for_clients_to_open(nr_clients=1)
+        self.socket.run('wf-panel -c wf-shell.ini')
+        self.wait_for_clients_to_open(nr_clients=2)
+
+        # Potentially wait for resize
+        self.wait_for_clients(2)
+
+        info = self.socket.get_view_info('nil')
+        if not wi.check_geometry(0, 200, 1280, 520, info['geometry']):
+            return wt.Status.WRONG, 'weston-terminal has invalid geometry: ' + str(info['geometry'])
+
+        return wt.Status.OK, None
