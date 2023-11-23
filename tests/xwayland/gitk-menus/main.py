4b3269d3709454e44e78dbaad112692d00c9e822
Ilia Bozhinov <ammen99@gmail.com>
2023-11-23 16:20:59 +0100

    gitk: add a bit of testing for wayfire #2007

--- tests/xwayland/gitk-menus/main.py
+++ tests/xwayland/gitk-menus/main.py
@@ -25,6 +25,10 @@ class WTest(wt.WayfireTest):
         self.socket.click_button('BTN_LEFT', 'full')
         self.wait_for_clients(2)
 
+        v = self.socket.get_view_info_title("#bar#file")
+        if v and 'focusable' in v['state'] and v['state']['focusable']:
+            return wt.Status.WRONG, "Wrong focusable state for gitk menu, should be false! (Wayfire #2007)"
+
         for i in range(1, 10):
             self.socket.move_cursor(i * 10, 10)
             self.wait_for_clients()
