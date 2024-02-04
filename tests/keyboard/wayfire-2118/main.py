736742f3872087a7d9060eafae78cdcdf074d727
Ilia Bozhinov <ammen99@gmail.com>
2024-02-04 22:03:21 +0100

    add wayfire-2118

---
+++ tests/keyboard/wayfire-2118/main.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+import wftest as wt
+def is_gui() -> bool:
+    return False
+
+# Wayfire #2118
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['weston-terminal', 'wf-panel'])
+
+    def _get_focused(self):
+        focused = self.socket.ipc_rules_get_focused()['info']
+        return focused['app-id'] if focused else 'None'
+
+    def _run(self):
+        self.socket.run('weston-terminal')
+        self.socket.run('wf-panel -c wf-shell.ini')
+        self.wait_for_clients_to_open(nr_clients=2)
+
+        layout = {}
+        layout['nil'] = (100, 100, 900, 900)
+        self.socket.layout_views(layout)
+        self.wait_for_clients(2)
+
+        focus = self._get_focused()
+        if focus != 'nil':
+            return wt.Status.WRONG, f'weston-terminal should be focused in the beginning, instead focus is {focus}'
+
+        self.socket.press_key('KEY_A')
+        self.wait_for_clients(2)
+
+        focus = self._get_focused()
+        if focus != 'panel':
+            return wt.Status.WRONG, f'wf-panel menu did not get focus, instead focus is {focus}'
+
+        self.socket.press_key('KEY_A')
+        self.wait_for_clients(2)
+
+        focus = self._get_focused()
+        if focus != 'nil':
+            return wt.Status.WRONG, f'weston-terminal did not regain focus, instead focus is {focus}'
+
+        return wt.Status.OK, None
