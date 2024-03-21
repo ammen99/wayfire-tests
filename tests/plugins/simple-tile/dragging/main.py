d36a3f8f55dd031aab5e8b90ed66fdc8820df344
Ilia Bozhinov <ammen99@gmail.com>
2024-03-21 10:03:38 +0100

    enhance simple-tile/dragging

--- tests/plugins/simple-tile/dragging/main.py
+++ tests/plugins/simple-tile/dragging/main.py
@@ -25,6 +25,15 @@ class WTest(wt.WayfireTest):
 
         return None
 
+    def drag_window_to(self, winid, tx, ty, release=True):
+        info = self.socket.get_view_info_id(winid);
+        assert info
+        sx = info['bbox']['x'] + info['bbox']['width'] / 2
+        sy = info['bbox']['y'] + info['bbox']['height'] / 2
+        if info['output-name'] == 'WL-2':
+            sx += 500
+
+        self.click_and_drag('BTN_LEFT', sx, sy, tx, ty, release)
 
     def _run(self):
         # Tile wt1, wt2 on WL-1, wt3 on WL-2
@@ -83,12 +92,7 @@ class WTest(wt.WayfireTest):
         self.socket.press_key('KEY_S')
         self.wait_for_clients()
 
-        info = self.socket.get_view_info_id(gcs1_id)
-        assert info
-        sx = info['bbox']['x'] + info['bbox']['width'] / 2 + 500
-        sy = info['bbox']['y'] + info['bbox']['height'] / 2
-
-        self.click_and_drag('BTN_LEFT', sx, sy, 250, 495, release=False)
+        self.drag_window_to(gcs1_id, 250, 495, release=False)
         self.wait_ms(300) # Preview is hardcoded for 200ms, wait a bit more
 
         if error := self.take_screenshot('4-preview-after-scale'):
@@ -102,13 +106,37 @@ class WTest(wt.WayfireTest):
         if err := self.check([gcs1_id, gcs2_id, gcs3_id], [(0, 255, 500, 245), (0, 0, 500, 500), (0, 0, 500, 245)], ['WL-1', 'WL-2', 'WL-1']):
             return wt.Status.WRONG, err + " after drag from scale to WL-1."
 
-        self.click_and_drag('BTN_LEFT', 250, 150, 750, 5)
+        # Tile on the right
+        self.drag_window_to(gcs3_id, 750, 895)
         self.wait_for_clients()
 
         if error := self.take_screenshot('6-drag-to-scale'):
             return wt.Status.CRASHED, error
 
-        if err := self.check([gcs1_id, gcs2_id, gcs3_id], [(0, 0, 500, 500), (0, 255, 500, 245), (0, 0, 500, 245)], ['WL-1', 'WL-2', 'WL-2']):
+        if err := self.check([gcs1_id, gcs2_id, gcs3_id], [(0, 0, 500, 500), (0, 0, 500, 245), (0, 255, 500, 245)], ['WL-1', 'WL-2', 'WL-2']):
             return wt.Status.WRONG, err + " after drag from WL-1 to scale."
+
+        # Now, move all windows between scale instances
+        self.socket.move_cursor(250, 250)
+        self.socket.click_button('BTN_LEFT', 'full')
+        self.socket.press_key('KEY_S')
+        self.wait_for_clients()
+
+        self.drag_window_to(gcs2_id, 0, 0)
+        self.drag_window_to(gcs3_id, 0, 0)
+
+        if error := self.take_screenshot('7-drag-all-to-WL-1'):
+            return wt.Status.CRASHED, error
+
+        if err := self.check([gcs1_id, gcs2_id, gcs3_id], [(0, 0, 500, 161), (0, 171, 500, 157), (0, 338, 500, 162)], ['WL-1', 'WL-1', 'WL-1']):
+            return wt.Status.WRONG, err + " after drag all to WL-1."
+
+        # Check that we can drag to empty space
+        self.drag_window_to(gcs1_id, 750, 250)
+        if error := self.take_screenshot('8-drag-to-empty'):
+            return wt.Status.CRASHED, error
+
+        if err := self.check([gcs1_id, gcs2_id, gcs3_id], [(0, 0, 500, 500), (0, 0, 500, 245), (0, 255, 500, 245)], ['WL-2', 'WL-1', 'WL-1']):
+            return wt.Status.WRONG, err + " after drag to empty."
 
         return wt.Status.OK, None
