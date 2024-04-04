c0eab63509979d4d9bda51f6e4b1d118ef82a405
Ilia Bozhinov <ammen99@gmail.com>
2024-04-04 10:54:48 +0200

    update fuzz-test

--- repeat-fuzz.sh
+++ repeat-fuzz.sh
@@ -12,8 +12,8 @@ while [ $counter -lt 100 ]; do
     # Check if the log file contains the error string
     #if grep -q "shutting down" staging/fuzz-test/wayfire.log; then
     #    echo "Ignoring shutdown crash"
-    #if grep -q "ERROR: AddressSanitizer:" staging/fuzz-test/wayfire.log; then
-    if grep -q "runtime error" staging/fuzz-test/wayfire.log; then
+    if grep -q "ERROR: AddressSanitizer:" staging/fuzz-test/wayfire.log; then
+    #if grep -q "runtime error" staging/fuzz-test/wayfire.log; then
         # Append the log file to stress-test.log
         cat staging/fuzz-test/wayfire.log >> stress-test.log
         exit 0
--- staging/fuzz-test/fuzz.py
+++ staging/fuzz-test/fuzz.py
@@ -1,19 +1,19 @@
 from wayfire.ipc import sock
 import random
-from random import randint
+from random import randint, choice
 
 rand_seed = randint(0, 1000)
 random.seed(rand_seed)
 with open("/tmp/seeds", "a") as f:
     f.write(str(rand_seed) + "\n")
 
-# scale, expo, showdesktop, switcherview, cube and None for all plugins
-plugin = None
+# scale, expo, showdesktop, switcherview, cube,  autororate
+plugin = choice(["scale", "expo", "showdesktop", "switcherview", "cube", "autorotate", None])
 while True:
     try:
         number_of_views = randint(1, 20)
         number_of_executions = randint(10, 100)
-        random_time_to_wait = randint(200, 400)
+        random_time_to_wait = randint(20, 400)
         sock.test_wayfire(
             number_of_views, number_of_executions, random_time_to_wait, plugin
         )
