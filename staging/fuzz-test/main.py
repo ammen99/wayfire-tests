b90ad4cf81c828059e8f1c6f375fb6960a5c11a5
Ilia Bozhinov <ammen99@gmail.com>
2024-04-17 13:13:15 +0200

    fuzz-test: make test longer

--- staging/fuzz-test/main.py
+++ staging/fuzz-test/main.py
@@ -5,6 +5,7 @@ from time import sleep
 import os
 import signal
 import traceback
+import subprocess
 
 
 def is_gui() -> bool:
@@ -16,13 +17,19 @@ class WTest(wt.WayfireTest):
             self.run_wayfire(wayfire_path, log)
             self.socket.create_wayland_output()
             self.socket.run("kitty --hold -- sh fuzz.sh")
-            timeout = 7
+            timeout = 15
             sleep(timeout)
 
             assert self._wayfire_process
             os.killpg(self._wayfire_gid, signal.SIGINT)
             if status := self._wayfire_process.wait(5.0):
                 return wt.Status.CRASHED, "Wayfire process exited with status {}".format(status)
+
+        except subprocess.TimeoutExpired:
+            assert self._wayfire_process
+            print("Wayfire did not terminate after SIGINT! Process PID is {}, killing after 1 minute".format(self._wayfire_process.pid))
+            sleep(60.0)
+            return wt.Status.CRASHED, "Wayfire did not terminate!"
 
         except Exception as _:
             return wt.Status.CRASHED, "Wayfire or client socket crashed, " + traceback.format_exc()
