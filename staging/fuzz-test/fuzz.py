86fe9ed9300d500e025f629b57675ef453872dba
killown <systemofdown@gmai.com>
2024-08-21 09:22:07 -0300

    fix sum geometry resolution

--- staging/fuzz-test/fuzz.py
+++ staging/fuzz-test/fuzz.py
@@ -1,5 +1,7 @@
-from wayfire.ipc import sock
 from random import randint, choice
+from fuzzy import TestWayfire
+
+test = TestWayfire()
 
 # scale, expo, showdesktop, switcherview, cube,  autororate
 plugin = choice(["scale", "expo", "showdesktop", "switcherview", "cube", "autororate", None])
@@ -8,8 +10,9 @@ while True:
         number_of_views = randint(1, 20)
         number_of_executions = randint(10, 100)
         random_time_to_wait = randint(20, 400)
-        sock.test_wayfire(
+        test.test_wayfire(
             number_of_views, number_of_executions, random_time_to_wait, plugin
         )
-    except:
+    except Exception as e:
+        print(e)
         continue
