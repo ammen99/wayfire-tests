b0cedf310d21681e8e3ea01e3314f47732d0d8cf
Ilia Bozhinov <ammen99@gmail.com>
2025-07-09 14:39:54 +0200

    update wleird-slow-ack test

--- tests/clients/wleird-slow-ack/main.py
+++ tests/clients/wleird-slow-ack/main.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 import wftest as wt
+import signal
 
 def is_gui() -> bool:
     return True
@@ -11,7 +12,7 @@ class WTest(wt.WayfireTest):
         return self.require_test_clients(['wleird-slow-ack-configure'])
 
     def _run(self):
-        self.socket.run('wleird-slow-ack-configure 0 0') # default size 0, 0 means unmap on default size ...
+        _, pid = self.run_get_id('wleird-slow-ack-configure 0 0') # default size 0, 0 means unmap on default size ...
         self.wait_ms(1000)
 
         if err := self.take_screenshot('1-map-unmap'):
@@ -22,13 +23,16 @@ class WTest(wt.WayfireTest):
         self.socket.layout_views(layout)
         self.wait_ms(200)
 
+        # Configure unmapped view
         layout = {}
         layout['wleird-slow-ack-configure'] = (0, 0, 1000, 1000)
         self.socket.layout_views(layout)
         self.wait_ms(200)
-
         if err := self.take_screenshot('2-pending-resize'):
             return wt.Status.CRASHED, "Failed screenshot: ", str(err)
+
+        # Trigger the view again
+        self.send_signal(pid, signal.SIGUSR1)
 
         self.wait_ms(500)
         if err := self.take_screenshot('3-client-driven-resize'):
