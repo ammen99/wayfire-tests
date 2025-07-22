1ea4e3d371422e692144c5ca21f8be2348a7f088
Ilia Bozhinov <ammen99@gmail.com>
2025-07-23 00:05:31 +0200

    add wayfire-2733

---
+++ tests/plugins/wsets/wayfire-2733/main.py
@@ -0,0 +1,42 @@
+#!/bin/env python3
+
+import wftest as wt
+from wayfire.extra.ipc_utils import WayfireUtils
+from random import randint, choice
+
+def is_gui() -> bool:
+    return False
+
+# This test opens gtk-logger and sends it back and forth between outputs and workspace sets
+class WTest(wt.WayfireTest):
+    def prepare(self):
+        return self.require_test_clients(['kitty'])
+
+    def _run(self):
+        self.socket.create_wayland_output()
+        utils = WayfireUtils(self.socket.sock)
+
+        # ensure at least one kitty for choice() to work
+        self.run_get_id('kitty /bin/sh')
+        for i in range(50):
+            if i % 5 == 0:
+                self.socket.run('kitty /bin/sh')
+
+            cursor_pos_x, cursor_pos_y = self.socket.sock.get_cursor_position()
+            self.socket.stipc.click_and_drag(
+                "BTN_LEFT",
+                cursor_pos_x,
+                cursor_pos_y,
+                randint(1, 100000),
+                randint(1, 100000),
+                release=True,
+                steps=randint(10, 100),
+            )
+
+            views = self.socket.list_views()
+            random_view_id = choice([view['id'] for view in views if view["role"] == "toplevel"])
+            self.socket.sock.send_view_to_wset(random_view_id, randint(1, 2))
+            self.socket.sock.set_focus(random_view_id)
+            utils.center_cursor_on_view(random_view_id)
+
+        return wt.Status.OK, None
