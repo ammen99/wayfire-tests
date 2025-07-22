5ba146d8519e0ad7f13d02aeb3ddd420ea9804fd
Ilia Bozhinov <ammen99@gmail.com>
2025-07-23 00:15:18 +0200

    update test even more

--- tests/plugins/wsets/wayfire-2733/main.py
+++ tests/plugins/wsets/wayfire-2733/main.py
@@ -10,17 +10,17 @@ def is_gui() -> bool:
 # This test spams kitty terminal on two different outputs and moves terminals between outputs with wsets
 class WTest(wt.WayfireTest):
     def prepare(self):
-        return self.require_test_clients(['kitty'])
+        return self.require_test_clients(['weston-terminal'])
 
     def _run(self):
         self.socket.create_wayland_output()
         utils = WayfireUtils(self.socket.sock)
 
         # ensure at least one kitty for choice() to work
-        self.run_get_id('kitty /bin/sh')
         for i in range(50):
             if i % 5 == 0:
-                self.socket.run('kitty /bin/sh')
+                self.socket.delay_next_tx()
+                self.socket.run('weston-terminal --shell=/bin/sh')
 
             cursor_pos_x, cursor_pos_y = self.socket.sock.get_cursor_position()
             self.socket.stipc.click_and_drag(
@@ -34,9 +34,13 @@ class WTest(wt.WayfireTest):
             )
 
             views = self.socket.list_views()
-            random_view_id = choice([view['id'] for view in views if view["role"] == "toplevel"])
-            self.socket.sock.send_view_to_wset(random_view_id, randint(1, 2))
-            self.socket.sock.set_focus(random_view_id)
-            utils.center_cursor_on_view(random_view_id)
+            if views:
+                try:
+                    random_view_id = choice([view['id'] for view in views if view["role"] == "toplevel"])
+                    self.socket.sock.send_view_to_wset(random_view_id, randint(1, 2))
+                    self.socket.sock.set_focus(random_view_id)
+                    utils.center_cursor_on_view(random_view_id)
+                except:
+                    pass
 
         return wt.Status.OK, None
