685d638dada8e0c6b0c81b645b339f33d19f44b3
Ilia Bozhinov <ammen99@gmail.com>
2025-08-05 10:07:29 +0200

    make gtk4-dnd a bit more robust

--- tests/clients/gtk4-dnd-after-unmaximize/main.py
+++ tests/clients/gtk4-dnd-after-unmaximize/main.py
@@ -1,7 +1,6 @@
 #!/bin/env python3
 
 import wftest as wt
-import wfipclib as wi
 
 def is_gui() -> bool:
     return False
@@ -14,38 +13,36 @@ class WTest(wt.WayfireTest):
         return self.require_test_clients(['gtk4-demo', 'wf-panel'])
 
     def _run(self):
-        print("\n")
-        self.socket.run('gtk4-demo')
-        self.socket.run('wf-panel -c wf-shell.ini')
-        self.wait_for_clients(5)
+        gtk4_id, _ = self.run_get_id('gtk4-demo')
 
-        gtk4_demo_view_info = self.socket.get_view_info("org.gtk.Demo4")
+        gtk4_demo_view_info = self.socket.get_view_info_id(gtk4_id)
+        print(gtk4_demo_view_info)
         geometry1 = gtk4_demo_view_info["base-geometry"]
-        self.wait_for_clients(1)
 
-        self.socket.sock.assign_slot(gtk4_demo_view_info["id"], "slot_c")
-        self.wait_for_clients(15)
-        self.socket.sock.assign_slot(gtk4_demo_view_info["id"], "restore")
-        self.wait_for_clients(10)
+        self.socket.sock.assign_slot(gtk4_id, "slot_c") # type: ignore
+        self.wait_for_clients(4)
+        self.socket.sock.assign_slot(gtk4_id, "restore") # type: ignore
+        self.wait_for_clients(4)
 
-        self.socket.move_cursor(450, 125)
+        sx = gtk4_demo_view_info["bbox"]["x"] + gtk4_demo_view_info["bbox"]["width"] / 2
+        sy = gtk4_demo_view_info["bbox"]["y"] + 25
+
+        self.socket.move_cursor(sx, sy)
         self.socket.click_button('BTN_LEFT', 'press')
         self.wait_for_clients(2)
-        self.socket.move_cursor(500, 125)
+        self.socket.move_cursor(sx + 50, sy)
         self.wait_for_clients(2)
-        self.socket.move_cursor(550, 125)
+        self.socket.move_cursor(sx + 100, sy)
         self.wait_for_clients(2)
-        self.socket.move_cursor(500, 125)
+        self.socket.move_cursor(sx + 50, sy)
         self.wait_for_clients(2)
-        self.socket.move_cursor(450, 125)
+        self.socket.move_cursor(sx, sy)
         self.wait_for_clients(2)
         self.socket.click_button('BTN_LEFT', 'release')
         self.wait_for_clients(1)
 
-        gtk4_demo_view_info = self.socket.get_view_info("org.gtk.Demo4")
+        gtk4_demo_view_info = self.socket.get_view_info_id(gtk4_id)
         geometry2 = gtk4_demo_view_info["base-geometry"]
-        self.wait_for_clients(1)
-
         if geometry1["x"] != geometry2["x"] or geometry1["y"] != geometry2["y"]:
             return wt.Status.WRONG, "Geometry incorrect, should be the same position before move.\nNOTE: x1: " + \
                 str(geometry1["x"]) + " x2: " + str(geometry2["x"]) + " - y1: " + str(geometry1["y"]) + " y2: " + str(geometry2["y"])
