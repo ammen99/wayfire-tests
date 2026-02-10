28bfbe59b4a00b2db43ffa914ee6c32273ae7786
Ilia Bozhinov <ammen99@gmail.com>
2026-02-10 10:43:03 +0100

    make run_nested.sh more flexible

--- run_nested.sh
+++ run_nested.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Kill children on exit
 trap "trap - SIGTERM && pkill -P $$ && exit" SIGINT SIGTERM EXIT
@@ -9,7 +9,11 @@ export OLD_WAYLAND_DISPLAY=$WAYLAND_DISPLAY
 unset DISPLAY
 unset WAYLAND_DISPLAY
 
-WLR_RENDERER=gles2 WLR_RENDER_DRM_DEVICE=/dev/dri/renderD128 WLR_BACKENDS=headless wayfire -c miniconfig.ini &> /tmp/outerlog &
+export WAYFIRE_BIN="${WAYFIRE_BIN:-wayfire}"
+export DRM_DEVICE="${WLR_RENDER_DRM_DEVICE:-/dev/dri/renderD128}"
+export OUTER_LOG="${OUTER_LOG:-/tmp/outerlog}"
+
+WLR_RENDER_DRM_DEVICE=$DRM_DEVICE WLR_RENDERER=gles2 WLR_BACKENDS=headless $WAYFIRE_BIN -c miniconfig.ini &> $OUTER_LOG &
 sleep 1
 display=$(cat /tmp/outerlog | grep "Using socket name" | cut -d ' ' -f 9)
 
